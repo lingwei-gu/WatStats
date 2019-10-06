6f6447881581ca17bbd2ce7be483ee268682233b
Lingwei Gu <gulw2001@163.com>
2019-10-06 19:41:25 -0400

    Update login.py

--- watstats/login.py
+++ watstats/login.py
@@ -1,9 +1,14 @@
 from selenium import webdriver
-driver = webdriver.Firefox(executable_path='your_path')
+driver = webdriver.Firefox(executable_path='geckodriver')
+#make sure that geckodriver is included in your package!
+
 driver.get("https://watcard.uwaterloo.ca/OneWeb/Account/LogOn")
-elem_user = driver.find_element_by_name("Account")
+
+elem_user = driver.find_element_by_name("Account") #input account
 elem_user.send_keys("your_account")
-elem_pwd = driver.find_element_by_name("Password")
+
+elem_pwd = driver.find_element_by_name("Password") #input password
 elem_pwd.send_keys("your_password")
-elem = driver.find_element_by_tag_name("button")
+
+elem = driver.find_element_by_tag_name("button") #click button
 elem.click()
